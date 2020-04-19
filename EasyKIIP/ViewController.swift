44c45826214c27dd4caea350c8b7810166077979
Do Thanh Tuan <tuando@next-aeon.com>
2020-04-19 14:24:23 +0900

    Initial Commit

---
+++ EasyKIIP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyKIIP
+//
+//  Created by Real Life Swift on 2020/04/19.
+//  Copyright © 2020 Real Life Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ EasyKIIPTests/EasyKIIPTests.swift
@@ -0,0 +1,34 @@
+//
+//  EasyKIIPTests.swift
+//  EasyKIIPTests
+//
+//  Created by Real Life Swift on 2020/04/19.
+//  Copyright © 2020 Real Life Swift. All rights reserved.
+//
+
+import XCTest
+@testable import EasyKIIP
+
+class EasyKIIPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
