a13f2b8e5e5bcb7a0ca0d39dc4d7a5de7524f503
Tuan Do <tuan.back@gmail.com>
2020-04-21 06:32:40 +0900

    + Write more test for practice history class

--- EasyKIIPKit/DataLayer/Model/Entity/PraticeHistory.swift
+++ EasyKIIPKit/DataLayer/Model/Entity/PraticeHistory.swift
@@ -8,13 +8,18 @@
 
 import Foundation
 
-struct PracticeHistory {
-  var isLearned: Bool
-  var numberOfTestTaken: UInt
-  var numberOfCorrectAnswer: UInt
-  var numberOfWrongAnswer: UInt
-  var lastTimeTest: Date
-  var proficiency: UInt8
+enum AppError: Error {
+  case invalidData
+}
+
+public struct PracticeHistory {
+  private(set) var isLearned: Bool
+  private(set) var numberOfTestTaken: UInt
+  private(set) var numberOfCorrectAnswer: UInt
+  private(set) var numberOfWrongAnswer: UInt
+  private(set) var lastTimeTest: Date
+  private(set) var proficiency: UInt8
+  private(set) var isMastered: Bool
   
   init() {
     self.isLearned = false
@@ -23,5 +28,39 @@ struct PracticeHistory {
     self.numberOfWrongAnswer = 0
     self.lastTimeTest = Date()
     self.proficiency = 0
+    self.isMastered = false
+  }
+  
+  /// Mark a vocab as mastered, it means, user doesn't want the app to show the vocab for practice
+  mutating func markAsIsMastered() {
+    self.isMastered = true
+    self.isLearned = true
+    self.proficiency = 100
+  }
+  
+  mutating func increaseNumberOfCorrectAnswerByOne() {
+    self.numberOfCorrectAnswer += 1
+    self.numberOfTestTaken += 1
+    self.lastTimeTest = Date()
+  }
+  
+  mutating func increaseNumberOfWrongAnswerByOne() {
+    self.numberOfWrongAnswer += 1
+    self.numberOfTestTaken += 1
+    self.lastTimeTest = Date()
+  }
+  
+  /// Set taken test data, should be used when we want to update the test taken data that may come from different source. Number of wrong answer will be calculated based on test taken and correct answer
+  /// - Parameters:
+  ///   - numberOfTakenTest: number of test has been taken
+  ///   - numberOfCorrectAnswer: number of correct answer
+  mutating func setTestData(numberOfTestTaken: UInt,
+                            numberOfCorrectAnswer: UInt) throws {
+    guard numberOfTestTaken >= numberOfCorrectAnswer else {
+      throw AppError.invalidData
+    }
+    self.numberOfTestTaken = numberOfTestTaken
+    self.numberOfCorrectAnswer = numberOfCorrectAnswer
+    self.numberOfWrongAnswer = numberOfTestTaken - numberOfCorrectAnswer
   }
 }
--- EasyKIIPKitTests/DataLayer/Model/Entity/PracticeHistoryTests.swift
+++ EasyKIIPKitTests/DataLayer/Model/Entity/PracticeHistoryTests.swift
@@ -38,9 +38,93 @@ class PracticeHistoryTests: XCTestCase {
     XCTAssertTrue(Date() >= sut.lastTimeTest)
   }
   
+  func test_increaseNumberCorrectAnswer2Times_SetNumberOfCorrectAnswerTo2() {
+    var sut = PracticeHistory()
+    
+    sut.increaseNumberOfCorrectAnswerByOne()
+    sut.increaseNumberOfCorrectAnswerByOne()
+    
+    XCTAssertEqual(sut.numberOfCorrectAnswer, 2)
+  }
+  
+  func test_increaseNumberWrongAnswer2Times_SetNumberOfWrongAnswerTo2() {
+    var sut = PracticeHistory()
+    
+    sut.increaseNumberOfWrongAnswerByOne()
+    sut.increaseNumberOfWrongAnswerByOne()
+    
+    XCTAssertEqual(sut.numberOfWrongAnswer, 2)
+  }
+  
+  func test_increaseNumberOfCorrectAnswer1Time_numberOfWrongAnswer1Time_IncreaseNumberOfTestTakenTo2() {
+    var sut = PracticeHistory()
+    
+    sut.increaseNumberOfCorrectAnswerByOne()
+    sut.increaseNumberOfWrongAnswerByOne()
+    
+    XCTAssertEqual(sut.numberOfTestTaken, 2)
+  }
+  
+  func test_increaseNumberOfCorrectAnswer2Times_numberOfWrongAnswer1Time_IncreaseNumberOfTestTakenTo3() {
+    var sut = PracticeHistory()
+    
+    sut.increaseNumberOfCorrectAnswerByOne()
+    sut.increaseNumberOfCorrectAnswerByOne()
+    sut.increaseNumberOfWrongAnswerByOne()
+    
+    XCTAssertEqual(sut.numberOfTestTaken, 3)
+  }
+  
+  func test_increaseNumberOfCorrectAnswer2Times_numberOfWrongAnswer2Times_IncreaseNumberOfTestTakenTo4() {
+    var sut = PracticeHistory()
+    
+    sut.increaseNumberOfCorrectAnswerByOne()
+    sut.increaseNumberOfWrongAnswerByOne()
+    sut.increaseNumberOfCorrectAnswerByOne()
+    sut.increaseNumberOfWrongAnswerByOne()
+    
+    XCTAssertEqual(sut.numberOfTestTaken, 4)
+  }
+  
+  func test_changeNumberOfCorrectAndWrongAnswer_changeLastTimeTest() {
+    
+    let timeBeforeTest = Date()
+    
+    var sut = PracticeHistory()
+    sut.increaseNumberOfWrongAnswerByOne()
+    sut.increaseNumberOfCorrectAnswerByOne()
+    
+    XCTAssertTrue(sut.lastTimeTest >= timeBeforeTest && sut.lastTimeTest <= Date())
+  }
+  
+  func test_setNumberOfTest_Correct_WrongAnswers_setValuesCorrectly() throws {
+    
+    let numberOfTestTaken = UInt.random(in: 0...10)
+    let numberOfCorrectAnswer = UInt.random(in: 0...numberOfTestTaken)
+    
+    var sut = PracticeHistory()
+    try sut.setTestData(numberOfTestTaken: numberOfTestTaken,
+                    numberOfCorrectAnswer: numberOfCorrectAnswer)
+    
+    XCTAssertEqual(sut.numberOfTestTaken, numberOfTestTaken)
+    XCTAssertEqual(sut.numberOfCorrectAnswer, numberOfCorrectAnswer)
+    XCTAssertEqual(sut.numberOfWrongAnswer, numberOfTestTaken - numberOfCorrectAnswer)
+  }
+  
+  // Test Proficiency Calculation
   func test_init_setProficiencyTo0() {
     let sut = PracticeHistory()
     XCTAssertEqual(sut.proficiency, 0)
+  }
+  
+  func test_markAsIsMastered_setIsLearnedAndProficiencyTo100() {
+    var sut = PracticeHistory()
+    
+    sut.markAsIsMastered()
+    
+    XCTAssertEqual(sut.isMastered, true)
+    XCTAssertEqual(sut.isLearned, true)
+    XCTAssertEqual(sut.proficiency, 100)
   }
   
 }
